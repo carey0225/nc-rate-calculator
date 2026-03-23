7479a65a55c593e9b711568e6d5704fe348ace70
Jerry Carey <jerry@energync.org>
2026-03-23 11:43:11 -0400

    fixed router context error in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import DeepDive from './DeepDive.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        {/* This is the Home Page */}
+        <Route path="/" element={<App />} />
+        
+        {/* This is the Second Page */}
+        <Route path="/deep-dive" element={<DeepDive />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
